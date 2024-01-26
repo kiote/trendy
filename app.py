a13e147450a0c1337be7ceff35b978ca5b883020
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2024-01-26 15:52:56 +0200

    Add file serving routes for frontends
    Update HTML link to CSS file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # simple_api.py
-from flask import Flask, jsonify
+import os.path
+from flask import Flask, jsonify, Response
 from flask_cors import CORS
 from data_sources.trends import get_trending_repos
 from data_sources.arxiv import fetch_archive
@@ -7,7 +8,22 @@ from data_sources.arxiv import fetch_archive
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
 @app.route('/daily', methods=['GET'])
 def home():
     return jsonify(get_trending_repos())
@@ -19,6 +35,16 @@ def weekly():
 @app.route('/monthly', methods=['GET'])
 def monthly():
     return jsonify(get_trending_repos(since='monthly'))
+
+@app.route('/')
+def metrics():  # pragma: no cover
+    content = get_file('frontends/arxiv/index.html')
+    return Response(content, mimetype="text/html")
+
+@app.route('/output.css')
+def output_css():
+    content = get_file('frontends/arxiv/src/output.css')
+    return Response(content, mimetype="text/css")
 
 @app.route('/arxiv/affective_computing', methods=['GET'])
 def arxiv_affective_computing():
